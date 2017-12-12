c426600a535a60eccafb2c1224f91d29942027df
DremusPro <33332438+DremusPro@users.noreply.github.com>
2017-12-12 15:38:22 -0600

    Java code for contact page

---
+++ src/contactPackage/Contact.java
@@ -0,0 +1,39 @@
+package contactPackage;
+
+public class Contact {
+	
+	/** member variables */
+	private String contactName;
+	private String contactEmail;
+	private String contactSubject;
+	private String contactMessage;
+	// if email were to be added, these are the fields
+	
+	/** member setters */
+	public void setContactName(String name) {
+		contactName = name;
+	}
+	public void setContactEmail(String email) {
+		contactEmail = email;
+	}
+	public void setContactSubject(String subject) {
+		contactSubject = subject;
+	}
+	public void setContactMessage(String message) {
+		contactMessage = message;
+	}
+	
+	/** member getters */
+	public String getContactName() {
+		return contactName;
+	}
+	public String getContactEmail() {
+		return contactEmail;
+	}
+	public String getContactSubject() {
+		return contactSubject;
+	}
+	public String getContactMessage() {
+		return contactMessage;
+	}
+}
